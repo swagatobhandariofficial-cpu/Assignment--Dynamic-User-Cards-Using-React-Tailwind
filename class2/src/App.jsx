c539922aaa16754cfbe7b499e76e5e286e8cd744
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-02-27 12:28:44 +0530

    use state method2

--- class2/src/App.jsx
+++ class2/src/App.jsx
@@ -2,22 +2,19 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [king, setking] = useState('Swagato')
-  const [queen, setqueen] = useState('Jimmy')
-
-  const cngKing = () => {
-    setking('Tree')
-  }
-
-  const cngQueen = () => {
-    setqueen('Butterfly')
-  }
-
+  const [num, setnum] = useState(0)
   return (
     <div className='p-6'>
-      <h1 className='text-2xl'>{king} X {queen}</h1>
-      <button onClick={cngKing} className='active:scale-97 bg-orange-700 text-white px-3 py-1.5 m-1 font-bold text-xl rounded-2xl'>Change King</button>
-      <button onClick={cngQueen} className='active:scale-97 bg-pink-900 text-white px-3 py-1.5 m-1 font-bold text-xl rounded-2xl'>Change Queen</button>
+      <h1 className='text-2xl'>{num}</h1>
+      <button onClick={() => {
+        setnum(num + 10)
+      }}
+        className='active:scale-97 bg-orange-700 text-white px-3 py-1.5 m-1 font-bold text-xl rounded-2xl'>Add Num</button>
+
+        <button onClick={() => {
+        setnum(num - 10)
+      }}
+        className='active:scale-97 bg-orange-700 text-white px-3 py-1.5 m-1 font-bold text-xl rounded-2xl'>Decrease Num</button>
     </div>
   )
 }
