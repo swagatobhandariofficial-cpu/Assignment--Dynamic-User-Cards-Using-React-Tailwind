027a62748952bc0da6f91e7e7965ba27fd34867f
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-02-27 21:39:06 +0530

    state handling3

--- class2/src/App.jsx
+++ class2/src/App.jsx
@@ -1,20 +1,14 @@
 import React, { useState } from 'react'
 
 const App = () => {
-
-  const [num, setnum] = useState(0)
+  const [num, setNum] = useState(0)
   return (
-    <div className='p-6'>
-      <h1 className='text-2xl'>{num}</h1>
-      <button onClick={() => {
-        setnum(num + 10)
-      }}
-        className='active:scale-97 bg-orange-700 text-white px-3 py-1.5 m-1 font-bold text-xl rounded-2xl'>Add Num</button>
-
-        <button onClick={() => {
-        setnum(num - 10)
-      }}
-        className='active:scale-97 bg-orange-700 text-white px-3 py-1.5 m-1 font-bold text-xl rounded-2xl'>Decrease Num</button>
+    <div >
+      <div className="box ">{num}</div>
+      <button className='bg-blue-700 rounded-sm' onClick={()=>{
+        const rend = Math.floor(Math.random()*100)
+        setNum(rend)
+      }}>Change Number</button>
     </div>
   )
 }
